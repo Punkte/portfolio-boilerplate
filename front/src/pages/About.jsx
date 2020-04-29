98dc58f4f54d8f14059705368b500560798360ea
maximeoger <maximeoger93@gmail.com>
2020-04-29 20:10:02 +0200

    fixed layout

---
+++ front/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import react, {Fragment} from 'react';
+import LCaption from '../components/layouts/LCaption';
+import Title from '../components/';
+
+
+const About = (props) => {
+	return (
+		<>
+			yo
+		</>
+	)
+};
+
+export default About;
