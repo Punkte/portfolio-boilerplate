b636ee812c113ec7b670c26a456a225e677d65c1
ptp <paartheepan94@gmail.com>
2020-04-30 16:48:16 +0200

    fix: duplicate variable

--- front/src/tests/story.test.js
+++ front/src/tests/story.test.js
@@ -3,11 +3,11 @@ import { render } from '@testing-library/react';
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect';
 
-import { Story } from '../stories/story/story.stories.js';
+import { Story as StoryComp } from '../stories/story/story.stories.js';
 
 describe('Renders Story component', () => {
     test('with default', () => {
-        const { container } = render(<Story text="coucou"/>);
+        const { container } = render(<StoryComp text="coucou"/>);
         expect(container).toBeTruthy();
     });
 });
